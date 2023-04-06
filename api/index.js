f0cff59436c4db6e4503e9185d1d36ec34d4e3a8
Oleksandr Dashko <leksdashko@gmail.com>
2023-04-06 14:43:34 -0700

    error middleware handler

--- api/index.js
+++ api/index.js
@@ -23,10 +23,25 @@ mongoose.connection.on("disconnected", () => {
 });
 
 //middlewares
+
+app.use(express.json());
+
 app.use('/api/auth', authRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/hotels', hotelsRouter);
 app.use('/api/rooms', roomsRouter);
+
+app.use((err, req, res, next) => {
+	const errorStatus = err.status || 500;
+	const errorMessage = err.message || "Something went wrong!";
+
+	return res.status(errorStatus).json({
+		success: false,
+		status: errorStatus,
+		message: errorMessage,
+		stack: err.stack
+	});
+});
 
 app.listen(8800, () => {
 	connect();
