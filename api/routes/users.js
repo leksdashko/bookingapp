8f3abda22b0b6ffb56b6be950458e281f7143137
Oleksandr Dashko <leksdashko@gmail.com>
2023-04-08 13:25:12 -0700

    token verifier

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,13 +1,19 @@
 const express = require('express');
+const { updateUser, deleteUser, getUser, getUsers } = require('../controllers/user');
+
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-	res.send('auth');
-});
-router.get('/register', (req, res) => {
-	res.send('register');
-});
+//UPDATE
+router.put('/:id', updateUser);
 
+//DELETE
+router.delete('/:id', deleteUser);
+
+//GET
+router.get('/:id', getUser);
+
+//GET ALL
+router.get('/', getUsers);
 
 module.exports = router;
